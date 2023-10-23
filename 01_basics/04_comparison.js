9ea45483eaecb61932b522c6ac316dc1adeb26ad
M-Meeran-Siddiqui <137807639+M-Meeran-Siddiqui@users.noreply.github.com>
2023-10-23 05:49:06 +0000

    Comparsion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//Conversion of Different types.
+
+// console.log("2" > 1);
+// console.log("02"> 1);
+
+// Note : for comparison after take a same type of data and also note that sometime js automatically convert the 2 as number for 
+//  correct result after comparison.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// ===
+
+console.log("2" === 2);
+
+
