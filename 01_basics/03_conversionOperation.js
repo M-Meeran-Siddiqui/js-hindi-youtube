ceeb7e05c101d4396d49778069bae9561796e5c3
M-Meeran-Siddiqui <137807639+M-Meeran-Siddiqui@users.noreply.github.com>
2023-10-23 05:24:47 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,12 +4,12 @@
 // let score = true;
 let score = "Meeran";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 //Note: When you get <NaN> then remember that it is also a type and used to check whether the number is in the form that is =>"33abc";
 
@@ -31,5 +31,38 @@ console.log(valueInNumber);
 
 let someNumber = 33;
 let stringNumber  = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************** Operations ****************************************
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+console.log(2**3);  // it is just like a power operator which gives the power  of the given number . 
+
+let str1  = "hello"
+let str2  = " Meeran"
+ 
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1"+ 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// gameCounter++;
+console.log(++gameCounter);
+console.log(gameCounter++);
+
+//Link to study https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
